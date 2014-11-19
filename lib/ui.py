59f4d4b65d40414c50d15ff2108eade3349cbad3
Dong Zhu <donzhu@cisco.com>
2014-11-19 15:41:30 +0800

    lib/ui.py: bugs fixing
    Signed-off-by: Dong Zhu <donzhu@cisco.com>

--- lib/ui.py
+++ lib/ui.py
@@ -103,14 +103,14 @@ class UCSM_GUI(UCSM_LOG_PARSE):
         return scrolled_window, self.buffer
 
     def __parse_error_log(self, treestore):
-        self.error_information(com_content['`show fault detail`'])
+        self.error_information(self.com_content['`show fault detail`'])
         for priority in self.Fault_Detail.keys():
             p = treestore.append(None, [priority])
             for id_num in sorted(self.Fault_Detail[priority].keys()):
                 treestore.append(p, [id_num])
 
     def __parse_event_log(self, treestore):
-        self.event_information(com_content['`show event detail`'])
+        self.event_information(self.com_content['`show event detail`'])
         for time_stamp in sorted(self.Event_Detail.keys()):
             treestore.append(None, [time_stamp])
 
